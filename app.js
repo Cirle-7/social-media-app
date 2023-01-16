8372f4b701fae70d93ae93bbd36cedaf834cd6eb
anslemkelechi <kelechi.webdev@gmail.com>
2023-01-16 01:41:56 +0100

    Structured MVC, Added new entry file

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
-
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-    console.log('Server is running...');
-})
+module.exports = app;
