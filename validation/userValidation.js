f3e99d98a5793cbc13d2e34bb93fdc05f00e4c2f
KorkuTegbe <korkusco@gmail.com>
2023-01-18 03:55:36 +0000

    Add more properties to user model and validator

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -2,8 +2,23 @@ const Joi = require('joi')
 
 
 const userValidator = Joi.object({
-    username: Joi.string(),
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
     password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+    email: Joi.string()
+        .email(),
+        
+    displayName: Joi.string()
+        .alphanum()
+        .min(1)
+        .max(30)
+        .required()
 })
 
 
