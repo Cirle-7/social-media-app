83ea32cfedba1b6ab1f46d6cc089f939229f4964
ssamuel <molubode81@gmail.com>
2023-01-22 22:16:49 +0100

    Add social login provider

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const appError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const logger = require("./utils/logger");
 const morganMiddleware = require("./utils/morgan");
+const { requiresAuth } = require('express-openid-connect')
+const authConfig  = require('./utils/auth0')
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+// auth configuration
+app.use(authConfig);
 
 //REGISTER MORGAN MIDDLEWARE
 app.use(morganMiddleware);
@@ -12,6 +20,14 @@ app.use(morganMiddleware);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+app.get('/', (req, res) => {
+  res.send( 'Welcome to our app')
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send( req.oidc.user ? 'Welcome': 'Login')
 });
 
 //HANDLE UNKNOWN REQUEST ERRORS
