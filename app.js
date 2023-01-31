a4af7b7c7d3716377369af40d60cf7f011c6c069
anslemkelechi <kelechi.webdev@gmail.com>
2023-01-31 20:17:29 +0100

    Microsoft Azure Setup Added

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(session({
-  secret: process.env.SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 })
--- utils/passportOAuth.js
+++ utils/passportOAuth.js
@@ -1,63 +1,68 @@
-const passport = require('passport')
-require('dotenv').config()
+const passport = require("passport");
+require("dotenv").config();
 
 //OAuthController
-const { checkOrCreateOAuthUser } = require('../controllers/authController')
+const { checkOrCreateOAuthUser } = require("../controllers/authController");
 
 //STARTEGIES
-const GoogleStrategy = require('passport-google-oauth2').Strategy
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
-const GithubStrategy = require('passport-github2').Strategy
-
+const GithubStrategy = require("passport-github2").Strategy;
 
 // GOOGLE STARTEGY
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3310/api/v1/users/auth/google/callback',
-    passReqToCallback: true
-}, async (req, accessToken, refreshToken, profile, done) => {
-
-    const googleDetails = {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3310/api/v1/users/auth/google/callback",
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      const googleDetails = {
         socialId: profile.id,
         displayName: profile.displayName,
         email: profile.email,
-        username: profile.displayName
+        username: profile.displayName,
+      };
+
+      // Check if user exist or Create user
+      await checkOrCreateOAuthUser(googleDetails);
+
+      done(null, profile);
     }
-
-    // Check if user exist or Create user
-    await checkOrCreateOAuthUser(googleDetails)
-
-    done(null, profile)
-}
-))
+  )
+);
 
 // GITHUB STARTEGY
-passport.use(new GithubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3310/api/v1/users/auth/github/callback',
-    passReqToCallback: true
-}, async (req, accessToken, refreshToken, profile, done) => {
-
-    const githubDetails = {
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3310/api/v1/users/auth/github/callback",
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      const githubDetails = {
         socialId: profile.id,
         displayName: profile.displayName,
         email: profile.email,
-        username: profile.username
+        username: profile.username,
+      };
+
+      // Check if user exist or Create user
+      await checkOrCreateOAuthUser(githubDetails);
+
+      done(null, profile);
     }
-
-    // Check if user exist or Create user
-    await checkOrCreateOAuthUser(githubDetails)
-
-    done(null, profile)
-}
-))
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user)
-})
+  done(null, user);
+});
 
 passport.deserializeUser((user, done) => {
-    done(null, user)
-})
+  done(null, user);
+});
