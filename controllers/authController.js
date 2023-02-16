c7ae86298bb626229d576eaf1848b2e177d5f86d
Ademeso josiah <68749841+ademesojosiah@users.noreply.github.com>
2023-02-16 18:08:07 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,9 +17,9 @@ const createSendToken = async (user, statusCode, res) => {
   const token = await user.createJwt();
   const cookieOptions = {
     expires: new Date(Date.now() + 1 * 60 * 60 * 1000),
-    httpOnly: true,
+    httpOnly: false,
   };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = false;
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
   // Send token to client
   res.cookie("jwt", token, cookieOptions);
