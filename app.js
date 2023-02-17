ede32c6d3667802d85a7dec99dac44dbffccdb80
anslemkelechi <kelechi.webdev@gmail.com>
2023-02-17 01:12:29 +0100

    Edit CORS Middleware

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const appError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const logger = require("./utils/logger");
 const morganMiddleware = require("./utils/morgan");
-const userRoute = require('./routes/userRoute')
-const postRoute = require('./routes/postRoute')
-const profileRoute = require('./routes/profileRoute')
-const followRoute = require('./routes/followRoute')
-const blockRoute = require('./routes/blockRoute')
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
+const profileRoute = require("./routes/profileRoute");
+const followRoute = require("./routes/followRoute");
+const blockRoute = require("./routes/blockRoute");
 const passport = require("passport");
-const cors = require("cors")
-require('./utils/passportOAuth')
+const cors = require("cors");
+require("./utils/passportOAuth");
 
 //import authentication
-const authentication = require('./middleware/authentication')
+const authentication = require("./middleware/authentication");
 
 //IMPLEMENT CORS
-app.use(cors({
-  credentials:true,
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'localhost'
-  ]
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 
 //VIEWS
-app.set('views', 'views');
-app.set('view engine', 'ejs');
-app.use(express.static('views'))
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.static("views"));
 
 //REGISTER MORGAN MIDDLEWARE
 app.use(morganMiddleware);
@@ -42,20 +40,19 @@ app.use(express.urlencoded({ extended: true }));
 // COOKIE PARSER
 app.use(cookieParser());
 
-
-
 // ROUTES
-app.use('/api/v1/users', userRoute)
-app.use('/api/v1/profiles', profileRoute)
-app.use('/api/v1/', followRoute)
-app.use('/api/v1/user/', blockRoute)
-app.use('/api/v1/post', authentication , postRoute)
-
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/profiles", profileRoute);
+app.use("/api/v1/", followRoute);
+app.use("/api/v1/user/", blockRoute);
+app.use("/api/v1/post", authentication, postRoute);
 
 //HOME ROUTE
-app.get('/', (req, res) => {
-  res.send("welcome to the social media app \n <a href='/api/v1/users/auth/google'>Continue with Google</a> <a href='/api/v1/users/auth/github'>Continue with Github</a>")
-})
+app.get("/", (req, res) => {
+  res.send(
+    "welcome to the social media app \n <a href='/api/v1/users/auth/google'>Continue with Google</a> <a href='/api/v1/users/auth/github'>Continue with Github</a>"
+  );
+});
 
 //CHNAGE REQUEST TIME FORMAT
 app.use((req, res, next) => {
