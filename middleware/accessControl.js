00bccc3d880899b15976b795fbadc2b09a546b67
KorkuTegbe <korkusco@gmail.com>
2023-02-23 07:22:50 +0000

    Restrict access to private accounts mw

--- middleware/accessControl.js
+++ middleware/accessControl.js
@@ -8,6 +8,7 @@ const appError = require('../utils/appError')
 const db = require('../models')
 const blockedAccounts = db.blockedAccounts;
 const User = db.users;
+const Followers = db.followers
 
 
 const accessControl = async (req,res,next) => {
@@ -43,7 +44,68 @@ const accessControl = async (req,res,next) => {
     }
 }
 
+/**
+ * This middleware checks if the user is authorized to view the private account by checking if the user is the owner of the account, is following the account or the account is public.
+*/
+const authorizeAccessToPrivateAccount = async (req, res, next) => {
+    const loggedInUserId = req.user.id //current user
+    const { username } = req.params  //account to access
+        
+    // // FIND USER 
+    const findUser = await User.findOne({ where: { username: username }})
+    const userId = findUser.id  //get id of found user
+
+    // ALLOW ACCESS TO OWN PRIVATE ACC
+    if(userId === loggedInUserId){
+        return next()
+    }
+
+    // CHECK IF USER IS FOLLOWER OF PRIVATE ACCOUNT 
+    const isFollower = await Followers.findOne({
+        where: {
+            followeeId: userId,
+            userId: loggedInUserId
+        }
+    })
+
+    // IF IS FOLLOWER ALLOW ACCESS TO PRIVATE ACCOUNT
+    if(isFollower){
+        return next()
+    }
+
+    // CHECK IF USER TO ACCESS IS PUBLIC ACCOUNT
+    const user = await User.findOne({
+        where: {
+            id: userId, //findUser.id
+            isPrivate: false
+        },
+    });
+
+    if(user){
+        return next()
+    }
+
+    return res.status(403).json({
+        message: 'Unauthorized access'
+    })
+}
+
+// FOR USERS WHO ARE NOT LOGGED IN
+const authorizeAccessToPrivateAccountForLoggedOutUsers = async (req, res, next) => {
+    const { username } = req.params  //account to access
+        
+    // // FIND USER 
+    const findUser = await User.findOne({ where: { username: username }})
+    const isPrivate = findUser.isPrivate === true //check if account is private
+    if(isPrivate){
+        return res.status(403).json({ message: `This is a private account. Follow user to get accesss`})
+    }
+    next()
+}
+
 
 module.exports = {
-    accessControl
+    accessControl,
+    authorizeAccessToPrivateAccount,
+    authorizeAccessToPrivateAccountForLoggedOutUsers
 }
--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -3,6 +3,7 @@ const profileController = require('../controllers/profileController')
 const upload = require('../config/upload')
 const authentication = require('../middleware/authentication')
 const { getLocation } = require('../middleware/getLocationMW')
+const { authorizeAccessToPrivateAccountForLoggedOutUsers,  } = require('../middleware/accessControl')
 
 router.post('/', 
     authentication, 
@@ -11,7 +12,7 @@ router.post('/',
     profileController.createProfile
 )
 router.patch('/:id', authentication, getLocation, upload.fields([{name:'avatar', maxCount: 1},{name:'header', maxCount: 1}]), profileController.updateProfile)
-router.get('/:username', profileController.getProfile)
+router.get('/:username', authorizeAccessToPrivateAccountForLoggedOutUsers, profileController.getProfile)  
 
 
 
