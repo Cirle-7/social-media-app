32559a41c43679d785c7a9a8111287748fdf0796
ademesojosiah <josiahademeso@gmail.com>
2023-02-25 21:31:54 +0100

    create logic,relationship between nested comments

--- models/index.js
+++ models/index.js
@@ -13,7 +13,6 @@ const userModel = require("./userModel");
 const postModel = require("./postModel");
 const profileModel = require("./profileModel");
 const commentModel = require("./commentModel");
-const commentCommentModel = require("./comment-commentModel");
 const followersModel = require("./followersModel");
 const blockedAccountsModel = require("./blockedAccountsModel");
 const likesModel = require("./likesModel");
@@ -40,7 +39,6 @@ db.users = userModel(sequelize, Sequelize.DataTypes);
 db.profile = profileModel(sequelize, Sequelize.DataTypes);
 db.post = postModel(sequelize, Sequelize.DataTypes);
 db.comments = commentModel(sequelize, Sequelize.DataTypes);
-db.commentsComments = commentCommentModel(sequelize, Sequelize.DataTypes);
 db.followers = followersModel(sequelize, Sequelize.DataTypes);
 db.blockedAccounts = blockedAccountsModel(sequelize, Sequelize.DataTypes);
 db.likes = likesModel(sequelize, Sequelize.DataTypes);
@@ -51,7 +49,6 @@ db.likes = likesModel(sequelize, Sequelize.DataTypes);
   let Profile = db.profile;
   let Posts = db.post;
   let comments = db.comments;
-  let commentsComments = db.commentsComments;
   let followers = db.followers;
   let blockedAccount = db.blockedAccounts;
   let likes = db.likes;
@@ -73,13 +70,11 @@ db.likes = likesModel(sequelize, Sequelize.DataTypes);
   Posts.hasMany(comments);
   comments.belongsTo(Posts);
 
-  // create a comment id in the comment_comment table
-  comments.hasMany(commentsComments);
-  commentsComments.belongsTo(comments);
 
-  // create a userId in the commentComments table
-  User.hasMany(commentsComments); // link commentsComments to their user
-  commentsComments.belongsTo(User);
+  // CREATE ASSOCIATION BETWEEN NESTED COMMENTS
+  comments.hasMany(comments);     // create a comment id in the comment table
+  comments.belongsTo(comments);
+
 
   // // create a userId in the followers table
   User.hasMany(followers);
@@ -90,17 +85,16 @@ db.likes = likesModel(sequelize, Sequelize.DataTypes);
   blockedAccount.belongsTo(User); //, { as: 'blockedUserId', foreignKey: 'blockedUser'})
 
   // CREATE ASSOCIATION BETWWEN LIKES AND USER
-  User.hasMany(likes);
+  User.hasMany(likes);    // CREATE A USERID IN THE LIKES TABLE
   likes.belongsTo(User);
 
+  // CREATE A POSTID IN THE LIKES TABLE
   Posts.hasMany(likes);
   likes.belongsTo(Posts);
 
-  comments.hasMany(likes)
+  comments.hasMany(likes)   // CREATE A COMMENTSID IN THE LIKES TABLE
   likes.belongsTo(comments)
 
-  commentsComments.hasMany(likes)
-  likes.belongsTo(commentsComments)
 
 })();
 
@@ -113,7 +107,7 @@ sequelize
 // sync the table
 db.sequelize
   .sync({ force: false })
-  //.sync({ alter: true })
+  // .sync({ alter: true })
   .then(() => logger.info("table sync successful"))
   .catch((err) => logger.error(err));
 
