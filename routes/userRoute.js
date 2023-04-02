8eb7b415ead1e6cfdb70fca2a5d0305f4664330b
Samuel <molubode81@gmail.com>
2023-04-02 17:19:53 +0100

    Fix: FE local redirection

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 const authController = require('../controllers/authController')
 const { userValidationMiddleware } = require('../validation/userValidation')
+require('dotenv')
 require('../utils/passportOAuth')
 const passport = require('passport')
 const { socialAuth } = require('../controllers/socialAuthController')
 const { checkActivation } = require('../middleware/reqReferer')
 const { getLocation } = require('../middleware/getLocationMW')
-const HOSTNAME = (process.env.NODE_ENV === 'production') ? 'https://www.circle7.codes' : `http://localhost:${process.env.PORT}`
 
-
+const redirectURL = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : 'https://test-social.vercel.app/feed'
 
 //AUTHENTIACTION ROUTES
 router.post("/signup", userValidationMiddleware, getLocation, authController.signup);
@@ -29,9 +29,9 @@ router.get('/auth/github', checkActivation, passport.authenticate('github'))
 
 
 //OAUTH CALLBACKS
-router.get('/auth/google/callback', passport.authenticate('google', { successRedirect: 'https://test-social.vercel.app/feed' }))
+router.get('/auth/google/callback', passport.authenticate('google', { successRedirect: redirectURL }))
 
-router.get('/auth/github/callback', passport.authenticate('github', { successRedirect: 'https://test-social.vercel.app/feed' }))
+router.get('/auth/github/callback', passport.authenticate('github', { successRedirect: redirectURL }))
 
 // REDIRECT URL
 router.get('/auth/login/success', socialAuth)
